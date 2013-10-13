361fc10b6212c1e930a045a97dc831f8eaaa18ec
prateek <prateek@prateek-PC>
2013-10-13 00:25:03 -0700

    just checking sales tool

---
+++ hieveryone/src/hieveryone/hi.java
@@ -0,0 +1,20 @@
+package hieveryone;
+
+public class hi {
+
+	public static void main(String[] args) {
+		
+		salesmandata data=new salesmandata();
+		displayGreeting();
+		data.display();
+		
+	}	
+		private static void displayGreeting(){
+				System.out.println("hello happy sales people");
+				System.out.println("this app shows sales data");
+			
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ hieveryone/src/hieveryone/salesmandata.java
@@ -0,0 +1,15 @@
+package hieveryone;
+
+public class salesmandata {
+	int data[]={3,7,25};
+
+public void display(){
+	System.out.println("implement data display");
+	
+	
+	
+	
+}
+	
+
+}
